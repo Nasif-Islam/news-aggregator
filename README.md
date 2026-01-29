31b0a4d03b89ce44e65ffd2ba5fefd960b624ac1
Nasif Islam <178942734+Nasif-Islam@users.noreply.github.com>
2026-01-29 11:49:30 +0000

    added readme and started seed function

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-# NC News Seeding
+# News Aggregator
 
-- Instructions for this sprint can be found at https://l2c.northcoders.com/courses/be/seeding-nc-news
+## How to set up the project
+
+- Ensure there is a .gitignore file that ignores the following files
+  - node_modules
+  - .env.\*
+
+- Run _npm install_ in the terminal to install the dependencies for the project
+
+- Set up the development and test databases using Postgres by running the terminal command _npm run setup-dbs_
+
+- Create two .env files at the root of the project
+  - .env.development
+  - .env.test
+
+- Add the names of the development and test database to the corresponding file
+  - PGDATABASE=development_database_name
+  - PGDATABASE=test_database_name
+
+- If connecting to another database e.g a cloud services such as DynamoDB, you may have to enter additional credentials
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,52 @@
-const db = require("../connection")
+const db = require("../connection");
 
-const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+const seed = async ({ topicData, userData, articleData, commentData }) => {
+  await db.query(`DROP TABLE IF EXISTS comments;`);
+  await db.query(`DROP TABLE IF EXISTS articles;`);
+  await db.query(`DROP TABLE IF EXISTS users;`);
+  await db.query(`DROP TABLE IF EXISTS topics;`);
+
+  const usersTablePromise = db.query(`
+    CREATE TABLE users (
+      username VARCHAR(255) PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      avatar_url VARCHAR(255) DEFAULT 'https://upload.wikimedia.org/wikipedia/commons/9/9c/Default_pfp.svg'
+    );
+    `);
+
+  const topicsTablePromise = db.query(`
+    CREATE TABLE topics (
+      slug VARCHAR(255) PRIMARY KEY,
+      description VARCHAR(255) NOT NULL,
+      img_url VARCHAR(255) DEFAULT 'https://images.pexels.com/photos/11035471/pexels-photo-11035471.jpeg'
+    );
+    `);
+
+  await Promise.all([usersTablePromise, topicsTablePromise]);
+
+  await db.query(`
+    CREATE TABLE articles (
+      article_id SERIAL PRIMARY KEY,
+      title VARCHAR(255) NOT NULL,
+      topic VARCHAR(255) NOT NULL REFERENCES topics(slug),
+      author VARCHAR(255) NOT NULL REFERENCES users(username),
+      body TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT NOW(),
+      votes INT DEFAULT 0,
+      article_img_url VARCHAR(255) DEFAULT 'https://images.pexels.com/photos/97050/pexels-photo-97050.jpeg?w=700&h=700'
+    );
+    `);
+
+  await db.query(`
+    CREATE TABLE comments (
+      comment_id SERIAL PRIMARY KEY,
+      article_id INT NOT NULL REFERENCES articles(article_id) ON DELETE CASCADE,
+      body TEXT NOT NULL,
+      votes INT DEFAULT 0,
+      author VARCHAR NOT NULL REFERENCES users(username),
+      created_at TIMESTAMP DEFAULT NOW()
+    );
+      `);
 };
+
 module.exports = seed;
