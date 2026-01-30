d1a863889b0b9cdd23147094212eae107d9b6967
Nasif Islam <178942734+Nasif-Islam@users.noreply.github.com>
2026-01-30 15:12:17 +0000

    added emoji article user data

--- db/data/data-utils.js
+++ db/data/data-utils.js
@@ -1,37 +1,38 @@
+const EMOJIS = [
+  "😀",
+  "🚀",
+  "🎉",
+  "🔥",
+  "🐱",
+  "🌮",
+  "😂",
+  "😍",
+  "😎",
+  "🤔",
+  "😭",
+  "🥳",
+  "🐶",
+  "🦄",
+  "🐙",
+  "🦕",
+  "🌵",
+  "🌈",
+  "🍕",
+  "🍔",
+  "🍩",
+  "🍺",
+  "☕",
+  "🥑",
+  "⚽",
+];
+
 function generateEmojiData(numOfRows) {
   const data = [];
-  const emojis = [
-    "😀",
-    "🚀",
-    "🎉",
-    "🔥",
-    "🐱",
-    "🌮",
-    "😂",
-    "😍",
-    "😎",
-    "🤔",
-    "😭",
-    "🥳",
-    "🐶",
-    "🦄",
-    "🐙",
-    "🦕",
-    "🌵",
-    "🌈",
-    "🍕",
-    "🍔",
-    "🍩",
-    "🍺",
-    "☕",
-    "🥑",
-    "⚽",
-  ];
 
   for (let i = 0; i < numOfRows; i++) {
     const record = {
       emoji_id: i + 1,
-      emoji: emojis[i % emojis.length],
+      emoji: EMOJIS[i % EMOJIS.length],
     };
     data.push(record);
   }
@@ -39,4 +40,22 @@ function generateEmojiData(numOfRows) {
   return data;
 }
 
-module.exports = { generateEmojiData };
+function generateEmojiArticleUser(numOfRows, maxEmojiId) {
+  const data = [];
+
+  for (let i = 0; i < numOfRows; i++) {
+    const record = {
+      emoji_article_user_id: i + 1,
+      emoji_id: Math.floor(Math.random() * maxEmojiId) + 1,
+      username: `user_${i + 1}`,
+      article_id: i + 1,
+    };
+    data.push(record);
+  }
+
+  return data;
+}
+
+console.log(generateEmojiArticleUser(25, 25));
+
+module.exports = { generateEmojiData, generateEmojiArticleUser };
