5d732b0d85aaf1c41692a168be112cf0c28c3655
Nasif Islam <178942734+Nasif-Islam@users.noreply.github.com>
2026-01-30 10:59:02 +0000

    queries complete

---
+++ db/queries.js
@@ -0,0 +1,84 @@
+const db = require("./connection");
+
+// Immediately Invoked Function Expression
+// Anonymous async function - wrap in brackets to make it an expressed, then call it immediately
+// (async () => {
+//   try {
+//     const users = await getUsers();
+//     console.log(users);
+//   } catch (err) {
+//     console.log(err);
+//   } finally {
+//     db.end();
+//   }
+// })();
+
+async function logQueryResults(promise) {
+  try {
+    const data = await promise;
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    db.end();
+  }
+}
+
+async function getUsers() {
+  const res = await db.query(`
+        SELECT username FROM users;
+        `);
+
+  return res.rows;
+}
+
+// logQueryResults(getUsers());
+
+async function getArticlesForATopic(topic) {
+  const res = await db.query(`SELECT title FROM articles WHERE topic = $1';`, [
+    topic,
+  ]);
+
+  return res.rows;
+}
+
+// logQueryResults(getArticlesForATopic());
+
+async function getComments(noOfVotes, operator) {
+  const operators = ["=", ">", "<", ">=", "<=", "!=", "<>"];
+  if (!operators.includes(operator)) return [];
+
+  const res = await db.query(
+    `SELECT * FROM comments WHERE votes ${operator} $1;`,
+    [noOfVotes],
+  );
+
+  return res.rows;
+}
+
+// logQueryResults(getComments(0, "<")); // negative comments
+
+async function getAllTopics() {
+  const res = await db.query(`
+    SELECT * FROM topics;
+    `);
+
+  return res.rows;
+}
+
+// logQueryResults(getAllTopics());
+
+async function getArticlesByUser(username) {
+  const res = await db.query(
+    `
+    SELECT * FROM articles WHERE author = $1;
+    `,
+    [username],
+  );
+
+  return res.rows;
+}
+
+// logQueryResults(getArticlesByUser("grumpy19"));
+
+logQueryResults(getComments(10, ">")); // negative comments
